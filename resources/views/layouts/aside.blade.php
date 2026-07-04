bdc5fa741e5158556c3cdfa8d3f93a2dd45cf7b9
Warrgyizmorsch <152847843+Warrgyizmorsch@users.noreply.github.com>
2026-07-04 16:16:12 +0530

    Update aside.blade.php

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -76,29 +76,7 @@
                     </div>
                 </div>
                 @endif
-                @if(auth()->check() && auth()->user()->role_id == 1)
-                    <div class="menu-item">
-                        <a class="menu-link {{ $isActiveRoute('subjects') ? 'active' : '' }}" href="{{ route('subjects.index') }}">
-                            <span class="menu-icon"><i class="fa fa-book"></i></span><span class="menu-title">Prefix</span>
-                        </a>
-                    </div>
-                    <div class="menu-item">
-                        <a class="menu-link {{ $isActiveRoute('service-pages') ? 'active' : '' }}" href="{{ route('service-pages.index') }}">
-                            <span class="menu-icon"><i class="fa fa-file-text-o"></i></span><span class="menu-title">Dynamic Page</span>
-                        </a>
-                    </div>
-                    <div class="menu-item">
-                        <a class="menu-link {{ $isActiveRoute('admin/login-otp-notifications') ? 'active' : '' }}" href="{{ route('admin.login-otp-notifications') }}">
-                            <span class="menu-icon"><i class="fa fa-bell"></i></span>
-                            <span class="menu-title">Login OTP</span>
-                            @if(($globalLoginOtpCount ?? 0) > 0)
-                                <span class="menu-badge">
-                                    <span class="badge badge-circle badge-danger fw-bold fs-8">{{ $globalLoginOtpCount }}</span>
-                                </span>
-                            @endif
-                        </a>
-                    </div>
-                @endif
+                
 
                 @foreach($premission as $permission)
                     @if(auth()->check() && auth()->user()->role_id == $permission->role_id)
