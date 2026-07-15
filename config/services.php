e07d3d7c6c8b96bf2ba88edfc32ac2a733756ef6
Warrgyizmorsch <152847843+Warrgyizmorsch@users.noreply.github.com>
2026-07-15 16:43:16 +0530

    a

--- config/services.php
+++ config/services.php
@@ -10,7 +10,8 @@ return [
     ],
 
     'mobile_app' => [
-        'password_reset_url' => env('MOBILE_PASSWORD_RESET_URL', 'ain://reset-password'),
+        'password_reset_url' => env('MOBILE_PASSWORD_RESET_URL', rtrim(env('APP_URL', ''), '/').'/app/reset-password'),
+        'deep_link' => env('MOBILE_PASSWORD_DEEP_LINK', 'ain://reset-password'),
     ],
 
     /*
