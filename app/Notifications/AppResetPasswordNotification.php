dc5723db19cfbd957dfae88ba1a18ea32e0573ad
Warrgyizmorsch <152847843+Warrgyizmorsch@users.noreply.github.com>
2026-07-15 16:52:54 +0530

    l;'

--- app/Notifications/AppResetPasswordNotification.php
+++ app/Notifications/AppResetPasswordNotification.php
@@ -21,10 +21,11 @@ class AppResetPasswordNotification extends Notification
 
     public function toMail(object $notifiable): MailMessage
     {
-        $resetUrl = route('password.reset', [
-            'token' => $this->token,
-            'email' => $notifiable->getEmailForPasswordReset(),
-        ]);
+        $resetUrl = rtrim(config('services.password_reset.web_url'), '/')
+            .'/'.rawurlencode($this->token)
+            .'?'.http_build_query([
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ]);
 
         return (new MailMessage)
             ->subject('Reset your AIN app password')
